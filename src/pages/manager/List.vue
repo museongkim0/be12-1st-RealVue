bedf10005f962f392d6138c352e0ab0fc9b4038c
gunha0405 <dhrjsgk0405@gmail.com>
2024-12-26 16:24:42 +0900

    [Feat] 매니저 대시보드 작업

---
+++ src/pages/manager/List.vue
@@ -0,0 +1,20 @@
+<script setup>
+import StudentList from './StudentList.vue';
+import InstructorList from './InstructorList.vue';
+import ManagerList from './ManagerList.vue';
+</script>
+
+<template>
+    <!--
+    <StudentList></StudentList>
+    <InstructorList></InstructorList>
+    <ManagerList/>
+    -->
+
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -9,16 +9,22 @@ import FormsView from '../pages/FormsView.vue';
 import SignupForm from '../pages/SignupForm.vue';
 import { h } from 'vue';
 import Studentdashboard from '../pages/Studentdashboard.vue';
-
-
-
+import ManagerDashboard from '../pages/manager/ManagerDashboard.vue';
+import List from '../pages/manager/List.vue';
+import StudentList from '../pages/manager/StudentList.vue';
+import InstructorList from '../pages/manager/InstructorList.vue';
+import ManagerList from '../pages/manager/ManagerList.vue';
 const router = createRouter({
     history: createWebHistory(),
     routes : [
         { path: '/a', component: FormsView },
         { path: '/b', component: LoginForm , meta: {hideAside: true , hideNav : true} },
         { path: '/c', component: SignupForm , meta: {hideAside: true , hideNav : true} },
-        {  path: '/d', component: Studentdashboard },
+        { path: '/d', component: Studentdashboard },
+        { path: '/manager/dashboard', component: ManagerDashboard },
+        { path: '/manager/studentList', component: StudentList },
+        { path: '/manager/instructorList', component: InstructorList },
+        { path: '/manager/managerList', component: ManagerList },
 
         // { path: '/c', component: Create },
         // { path: '/d', component: Signup },

00553abeff21e3ab43cb9e5ad989b8eae40d7990
museongkim <mstar0228@gmail.com>
2024-12-30 10:09:16 +0900

    [feat] 사이드바&헤더 업데이트 #23

--- src/pages/Instructor/InstructorDash.vue
+++ src/pages/Instructor/InstructorDash.vue
@@ -19,52 +19,55 @@ const studentCount = ref(5);
 </script>
 
 <template>
-    <section class="p-6 xl:max-w-6xl xl:mx-auto">
-        <div class="flex flex-col mb-6 bg-white rounded-lg p-4 shadow-md">
-            <div class="mb-4 flex justify-between items-center">
-               <h2 class="text-xl font-semibold text-gray-800 dark:text-white">커리큘럼</h2>
-               <button type="button" class="text-white bg-blue-500 hover:bg-blue-400 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 me-2 mb-2 dark:bg-blue-400 dark:hover:bg-blue-300 focus:outline-none dark:focus:ring-blue-600 flex items-center">
+    <div style="background-color: whitesmoke;"
+    class="xl:pl-60 pt-14 min-h-screen w-full transition-position bg-gray-50 dark:bg-slate-800 dark:text-slate-100">
+        <section class="p-6 xl:max-w-6xl xl:mx-auto">
+            <div class="flex flex-col mb-6 bg-white rounded-lg p-4 shadow-md">
+                <div class="mb-4 flex justify-between items-center">
+                <h2 class="text-xl font-semibold text-gray-800 dark:text-white">커리큘럼</h2>
+                <button type="button" class="text-white bg-blue-500 hover:bg-blue-400 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 me-2 mb-2 dark:bg-blue-400 dark:hover:bg-blue-300 focus:outline-none dark:focus:ring-blue-600 flex items-center">
+                        Create
+                        <svg class="w-4 h-4 text-white ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 7.757v8.486M7.757 12h8.486M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                        </svg>
+                    </button>
+                </div>
+                <div class="flex flex-wrap gap-4">
+                    <CourseCard v-for="student in studentCount" :key="student" class="mb-4"></CourseCard>
+                </div>
+            </div>
+            <div class="flex flex-col mb-6 bg-white rounded-lg p-4 shadow-md">
+                <div class="mb-4 flex justify-between items-center">
+                <h2 class="text-xl font-semibold text-gray-800 dark:text-white">과제 목록</h2>
+                <button type="button" class="text-white bg-blue-500 hover:bg-blue-400 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 me-2 mb-2 dark:bg-blue-400 dark:hover:bg-blue-300 focus:outline-none dark:focus:ring-blue-600 flex items-center">
                     Create
                     <svg class="w-4 h-4 text-white ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 7.757v8.486M7.757 12h8.486M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                     </svg>
-                </button>
+                    </button>
+                </div>
+                <TableHw></TableHw>  
             </div>
-            <div class="flex flex-wrap gap-4">
-                <CourseCard v-for="student in studentCount" :key="student" class="mb-4"></CourseCard>
+            <div class="flex flex-col mb-6 bg-white rounded-lg p-4 shadow-md">
+                <div class="mb-4 flex justify-between items-center">
+                <h2 class="text-xl font-semibold text-gray-800 dark:text-white">자료 목록</h2>
+                <button type="button" class="text-white bg-blue-500 hover:bg-blue-400 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 me-2 mb-2 dark:bg-blue-400 dark:hover:bg-blue-300 focus:outline-none dark:focus:ring-blue-600 flex items-center">
+                    Create
+                    <svg class="w-4 h-4 text-white ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 7.757v8.486M7.757 12h8.486M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                    </svg>
+                    </button>
+                </div>
+                <TableData></TableData>  
             </div>
-        </div>
-        <div class="flex flex-col mb-6 bg-white rounded-lg p-4 shadow-md">
-            <div class="mb-4 flex justify-between items-center">
-               <h2 class="text-xl font-semibold text-gray-800 dark:text-white">과제 목록</h2>
-               <button type="button" class="text-white bg-blue-500 hover:bg-blue-400 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 me-2 mb-2 dark:bg-blue-400 dark:hover:bg-blue-300 focus:outline-none dark:focus:ring-blue-600 flex items-center">
-                Create
-                <svg class="w-4 h-4 text-white ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 7.757v8.486M7.757 12h8.486M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-                </svg>
-                </button>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 mb-4 bg-white rounded-lg p-2 shadow-md">
+                <UserCard2
+                    v-for="student in instructorStore.student_list"
+                    :student="student"
+                ></UserCard2>
             </div>
-            <TableHw></TableHw>  
-        </div>
-        <div class="flex flex-col mb-6 bg-white rounded-lg p-4 shadow-md">
-            <div class="mb-4 flex justify-between items-center">
-               <h2 class="text-xl font-semibold text-gray-800 dark:text-white">자료 목록</h2>
-               <button type="button" class="text-white bg-blue-500 hover:bg-blue-400 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 me-2 mb-2 dark:bg-blue-400 dark:hover:bg-blue-300 focus:outline-none dark:focus:ring-blue-600 flex items-center">
-                Create
-                <svg class="w-4 h-4 text-white ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 7.757v8.486M7.757 12h8.486M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-                </svg>
-                </button>
-            </div>
-            <TableData></TableData>  
-        </div>
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 mb-4 bg-white rounded-lg p-2 shadow-md">
-            <UserCard2
-                v-for="student in instructorStore.student_list"
-                :student="student"
-            ></UserCard2>
-        </div>
-    </section>
+        </section>
+    </div>
 </template>
 
 <style scoped>

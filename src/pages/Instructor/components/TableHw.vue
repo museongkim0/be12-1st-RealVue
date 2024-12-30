a1027609a62b8060484005bf42e1dbb36468ce5a
museongkim <mstar0228@gmail.com>
2024-12-30 16:23:35 +0900

    [Feat] 삭제 팝업 추가 #42

--- src/pages/Instructor/components/TableHw.vue
+++ src/pages/Instructor/components/TableHw.vue
@@ -8,6 +8,8 @@ const instructorStore = useInstructorStore();
 
 const showPopup = ref(false); // 팝업 상태 관리
 const selectedRowData = ref(null); // 선택된 행의 데이터
+const showPopup2 = ref(false); // 팝업 상태 관리
+const selectedRowData2 = ref(null); // 선택된 행의 데이터
 
 onMounted(async () => {
     await instructorStore.fetchHw()
@@ -21,6 +23,15 @@ const openPopup = (row) => {
 
 const closePopup = () => {
     showPopup.value = false; // 팝업 닫기
+};
+
+const openPopup2 = (row) => {
+    selectedRowData2.value = row; // 선택된 데이터 설정
+    showPopup2.value = true; // 팝업 열기
+};
+
+const closePopup2 = () => {
+    showPopup2.value = false; // 팝업 닫기
 };
 
 </script>
@@ -54,23 +65,23 @@ const closePopup = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 cursor-pointer" v-for="hw in instructorStore.hw_list" @click="openPopup(hw)">
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" v-for="hw in instructorStore.hw_list">
                     <td class="w-4 p-4">
                         <div class="flex items-center">
                             <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                             <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
                         </div>
                     </td>
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white cursor-pointer" @click="openPopup(hw)">
                         {{hw.assignmentName}}
                     </th>
-                    <td class="px-6 py-4">
+                    <td class="px-6 py-4 cursor-pointer" @click="openPopup(hw)">
                         {{hw.fileName}}
                     </td>
-                    <td class="px-6 py-4">
+                    <td class="px-6 py-4 cursor-pointer" @click="openPopup(hw)">
                         {{hw.submissionRate}}
                     </td>
-                    <td class="px-6 py-4">
+                    <td class="px-6 py-4 cursor-pointer" @click="openPopup(hw)">
                         {{hw.viewCount}}
                     </td>
                     <td class="px-6 py-4">
@@ -79,7 +90,7 @@ const closePopup = () => {
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
                             </svg>    
                         </button>
-                        <button class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
+                        <button @click="openPopup2(hw.assignmentName)" class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
                             <svg class="w-4 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
                             </svg>
@@ -130,6 +141,30 @@ const closePopup = () => {
                     </button>
                 </div>
                 <TableHwUser></TableHwUser>
+            </div>
+        </div>
+    </div>
+
+    <!-- 삭제 팝업 -->
+    <div v-if="showPopup2" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+        <div class="relative p-4 w-full max-w-md max-h-full">
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <button type="button" @click="closePopup2" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+                <div class="p-4 md:p-5 text-center">
+                    <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                    </svg>
+                    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this homework?</h3>
+                    <button type="button" @click="closePopup2" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                        Yes, I'm sure
+                    </button>
+                    <button type="button" @click="closePopup2" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
+                </div>
             </div>
         </div>
     </div>
